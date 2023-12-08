51f54a67942c8ac2bc625126a3f9895c965bcd07
leefyl <leef_acc@tutanota.com>
2023-12-08 14:33:20 +0100

    add file

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # legendary-computing-machine
 test
+
+testing if I can update the read me from home pc
---
+++ rcode.r
@@ -0,0 +1,7 @@
+# a simple code
+a <- 12
+b <- 9
+if ((a + b) == 21){
+    print('test to update to github')
+}
+# all good
